b95d0302259f5addfd5379a1e0062eacd6169b25
isadoraalbano <isadoraalbano6@gmail.com>
2023-11-18 07:34:07 -0300

    create login, register and services pages

---
+++ server.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var url = require('url');
+
+app.use(express.static('public'));
+app.use(express.json());
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+var server = app.listen(3000, function () {
+   var host = server.address().address;
+   var port = server.address().port;
+   console.log("Example app listening at http://%s:%s", host, port);
+   console.log(server.address())
+});
+
