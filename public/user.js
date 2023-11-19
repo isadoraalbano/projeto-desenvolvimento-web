62eda75d94ab87760a5685f3d3472039401e9c57
isadoraalbano <isadoraalbano6@gmail.com>
2023-11-18 22:11:22 -0300

    add services page link to login button

--- public/user.js
+++ public/user.js
@@ -20,6 +20,9 @@ function login() {
     }
     $.post( "http://localhost:3000/login", data, function(resp) {
         alert( resp.msg );
+        $(document).on('click', '.btn', function() {
+                window.location = '/servicos.html'
+            })
       }).fail(function() {
           alert( "Email ou senha incorretos!" );
     })
