df1ce105a74de77cbf782932198fda9ee5d1c1db
isadoraalbano <isadoraalbano6@gmail.com>
2023-11-18 22:02:08 -0300

    update header and complete server

--- server.js
+++ server.js
@@ -10,25 +10,46 @@ var depoimentos = [
       texto: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
    }
 ]
+var users = [
+    {
+    email: "teste@email.com",
+    password: "senha123"
+    }
+];
 
 app.use(express.static('public'));
 app.use(express.json());
 
 app.use(bodyParser.urlencoded({extended: true}));
 
-// app.post('/user', function (req, res){
-//     var user = req.body;
-//     localStorage.setItem('email', user.email);
-//     localStorage.setItem('password', user.password);
-//     res.status(401).send(new Error('errooooo'))
-// })
+// Cadastro usuário
 
-var server = app.listen(3000, function () {
-   var host = server.address().address;
-   var port = server.address().port;
-   console.log("Example app listening at http://%s:%s", host, port);
-   console.log(server.address())
-});
+app.post("/user", function (req, res) {
+    var newUser = req.body;
+    users.push(newUser)
+    res.writeHead(200, {'Content-Type': 'application/json'});
+    res.end('{ "msg": "Usuário Cadastrado com sucesso!" }');
+})
+
+// Login Usuário
+
+app.post('/login', function (req, res){
+    var userExists = false;
+    var userLogin = req.body;
+    users.forEach(user => {
+        if(user.email == userLogin.email && user.password == userLogin.password){
+            userExists = true;
+        }
+    });
+    if(userExists){
+        res.writeHead(200, {'Content-Type': 'application/json'});
+        res.end('{ "msg": "Usuário logado!" }');
+    } else {
+        res.writeHead(401, {'Content-Type': 'application/json'});
+        res.status(401).send(new Error())
+    }
+
+})
 
 //Depoimentos
 app.get("/depoimento", (req, res) => {
@@ -55,3 +76,11 @@ app.post("/depoimento", (req, res) => {
 
    res.send(novoDepoimento);
 });
+
+var server = app.listen(3000, function () {
+    var host = server.address().address;
+    var port = server.address().port;
+    console.log("Example app listening at http://%s:%s", host, port);
+    console.log(server.address())
+ });
+ 
